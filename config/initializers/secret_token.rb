1be0922c46269832c8052a83d54f6aef82204534
kenokamoto <kenokmt@gmail.com>
2011-11-16 19:43:10 +0900

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Kakeibo::Application.config.secret_token = 'b34be27d0c60a9ff18b3b8cb8218f99711f6383659e378e5c32edbfb4706db7220c380db8e0df09a5d2d3aeebf6c9eb6964c593d4500d1a82d057c2b0f6cc6be'
